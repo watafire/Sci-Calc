35e0bfff262ccb2006246d3d8c8ffa5dff7497ce
Sean-Garnett <sean.m.garnett@gmail.com>
2017-03-04 18:56:32 -0500

    Added base operators to lib class, updated some comment text

--- Lib/src/lib/Lib.java
+++ Lib/src/lib/Lib.java
@@ -7,7 +7,7 @@ package lib;
 
 /**
  *
- * @author sean
+ * @author Sean Garnett, Jacob Ogden
  */
 public class Lib {
 
@@ -15,7 +15,32 @@ public class Lib {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println(multiply(1.0, 3));
         // TODO code application logic here
+    }
+    
+    public static int add(int a, int b) {
+        return a+b ;
+    }
+    
+    public static double add(double a, double b) {
+        return a+b;
+    }
+    
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
+    
+    public static double subtract(double a, double b) {
+        return a-b;
+    }
+    
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+    
+    public static double multiply(double a, double b) {
+        return a*b;
     }
     
 }
